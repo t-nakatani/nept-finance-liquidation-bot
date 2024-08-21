20d9ca7cc896291a7dd988ef7310fd6f7d567ecb
t-nakatani <tomoya.12@icloud.com>
2024-08-21 20:15:57 +0900

    fix: 不要なテスト用スクリプトを削除

--- src/market_manager/run_mm.ts
+++
@@ -1,9 +0,0 @@
-import { MarketManager } from "./market_manager";
-import { Network } from "@injectivelabs/networks";
-
-async function main() {
-  const manager = new MarketManager(Network.MainnetSentry);
-  await manager.fetchAccounts();
-}
-
-main().catch(console.error);
